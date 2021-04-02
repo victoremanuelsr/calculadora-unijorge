591cf383a3c922949c3d2a2b4ff8c85c9120b0df
-global <-global>
2021-04-02 13:07:26 -0300

    Funcionalidade JS

--- js/index.js
+++ js/index.js
@@ -1,17 +1,14 @@
-
-
 function calcular(){
     let notaA1 = document.getElementById('notaA1').value;
     let notaA2 = document.getElementById('notaA2').value;
-    let resultado = document.getElementById('resultado');
-    if(notaA1 == 0 || notaA2 < 5){
-        resultadoFinal();
-        // resultado.innerHTML = "<h4>" + `Suas notas não se enquadram as regras! Você está Reprovado` + "</h4>";
-    }else if(notaA1 > 0 && notaA2 >= 5 ){
-        let notaFinalPonderada = (notaA1 * 0.4) + (notaA2 * 0.6);  
-        function resultadoFinal(){
-            resultado.innerHTML = "<h4>" + `Sua nota foi ${notaFinalPonderada.toFixed(2)}, você está ${(notaFinalPonderada >= 6) ? "Aprovado" : "Reprovado"}` + "</h4>";
-        }  
-        resultadoFinal();  
+    let resultadoDiv = document.getElementById('resultado');
+    function resultadoFinal(){
+        if(notaA1 > 0 && notaA2 >= 5 ){
+            let notaFinalPonderada = (notaA1 * 0.4) + (notaA2 * 0.6);    
+            resultadoDiv.innerHTML = `<h4>Sua nota foi ${notaFinalPonderada.toFixed(2)}, você está ${(notaFinalPonderada >= 6) ? "Aprovado" : "Reprovado"}</h4>`;
+        }else{
+            resultadoDiv.innerHTML = `<h4>Sua nota não atende aos critérios, você está Reprovado </h4>`;
+        }
     }
+    resultadoFinal();         
 }   
