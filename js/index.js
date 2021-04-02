dcac1da2656e269bb2817a68fa33a57f819b4f50
-global <-global>
2021-04-02 12:36:03 -0300

    JS

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ function calcular(){
     let notaA2 = document.getElementById('notaA2').value;
     let resultado = document.getElementById('resultado');
     if(notaA1 == 0 || notaA2 < 5){
-        resultado.innerHTML = "<h4>" + `Suas notas não se enquadram as regras! Você está Reprovado` + "</h4>";
+        resultadoFinal();
+        // resultado.innerHTML = "<h4>" + `Suas notas não se enquadram as regras! Você está Reprovado` + "</h4>";
     }else if(notaA1 > 0 && notaA2 >= 5 ){
-        let notaFinalPonderada = (notaA1 * 0.4) + (notaA2 * 0.6);
+        let notaFinalPonderada = (notaA1 * 0.4) + (notaA2 * 0.6);  
         function resultadoFinal(){
-            (notaFinalPonderada >= 6) ? resultado.innerHTML = "<h4>" + `Sua nota foi ${notaFinalPonderada.toFixed(2)}, você está Aprovado!` + "</h4>"
-             : resultado.innerHTML = "<h4>" + `Sua nota foi ${notaFinalPonderada.toFixed(2)}, você está Reprovado!` + "</h4>"  
+            resultado.innerHTML = "<h4>" + `Sua nota foi ${notaFinalPonderada.toFixed(2)}, você está ${(notaFinalPonderada >= 6) ? "Aprovado" : "Reprovado"}` + "</h4>";
         }  
-        resultadoFinal();    
-    }    
+        resultadoFinal();  
+    }
 }   
