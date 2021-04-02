d07eb66155d56f8b25159493639774659afad6f5
-global <-global>
2021-04-02 12:01:44 -0300

    funcionalidades JS

--- js/index.js
+++ js/index.js
@@ -8,10 +8,10 @@ function calcular(){
         resultado.innerHTML = "<h4>" + `Suas notas não se enquadram as regras! Você está Reprovado` + "</h4>";
     }else if(notaA1 > 0 && notaA2 >= 5 ){
         let notaFinalPonderada = (notaA1 * 0.4) + (notaA2 * 0.6);
-        if(notaFinalPonderada >= 6){
-            resultado.innerHTML = "<h4>" + `Sua nota foi ${notaFinalPonderada.toFixed(2)}, você está Aprovado!` + "</h4>";
-        }else{
-            resultado.innerHTML = "<h4>" + `Sua nota foi ${notaFinalPonderada.toFixed(2)}, você está Reprovado!` + "</h4>";
+        function resultadoFinal(){
+            (notaFinalPonderada >= 6) ? resultado.innerHTML = "<h4>" + `Sua nota foi ${notaFinalPonderada.toFixed(2)}, você está Aprovado!` + "</h4>"
+             : resultado.innerHTML = "<h4>" + `Sua nota foi ${notaFinalPonderada.toFixed(2)}, você está Reprovado!` + "</h4>"  
         }  
-    }       
-}
+        resultadoFinal();    
+    }    
+}   
